e00e41c0334e17f89c14c3022d788712c2de05b7
Rupesh Merotha <rupeshmerotha2004@gmail.com>
2025-06-19 05:47:06 +0530

    pagination and feed api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const userAuth = require("../middleware/auth")
 const User = require("../models/user")
+const connectionRequest = require("../models/connectionRequest")
 const profileRouter = express.Router()
 
 profileRouter.get("/profile/view", userAuth , async (req,res)=>{
@@ -31,13 +32,35 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 });
 
 profileRouter.get("/profile/all", userAuth, async (req,res)=>{
-    try{
-        const allUsers = await User.find()
-        res.send(allUsers)
-    }
-    catch(err){
-        res.status(400).send("Unable to get users")
-    }
+    try {
+    const loggedInUserId = req._id;
+    const connectionRequests = await connectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUserId },
+        { toUserId: loggedInUserId }
+      ]
+    })
+
+    // 3. Create a set of user IDs to exclude from feed
+    const hideUsersFromFeed = new Set();
+    hideUsersFromFeed.add(loggedInUserId); // add yourself first
+
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId);
+      hideUsersFromFeed.add(req.toUserId);
+    });
+
+    // 4. Find all users NOT in the excluded set
+    const users = await User.find({
+      _id: { $nin: Array.from(hideUsersFromFeed) },
+    }).select("firstName lastName age gender skills about photoUrl"); // optional: only return needed fields
+
+    // 5. Send response
+    res.status(200).json(users);
+  } catch (err) {
+    console.error("Feed Error: ", err); 
+    res.status(500).send("Something went wrong while building your feed.");
+  }
     
 })
 
